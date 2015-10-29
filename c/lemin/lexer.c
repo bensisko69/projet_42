cb0b8bf76ac8e2d4e94814fb58c6d1bf8a1c3ef5
Loic RENOUD-GRAPPIN <lrenoud-@e1r10p8.42.fr>
2015-10-29 15:38:43 +0100

    modif

--- c/lemin/lexer.c
+++ c/lemin/lexer.c
@@ -6,7 +6,7 @@
 /*   By: lrenoud- <lrenoud-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/29 14:17:13 by lrenoud-          #+#    #+#             */
-/*   Updated: 2015/10/29 15:25:35 by lrenoud-         ###   ########.fr       */
+/*   Updated: 2015/10/29 15:38:36 by lrenoud-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,24 @@
 
 int		cmp(t_liste *liste)
 {
+	char	*str;
+	t_liste	*tmp;
 
-	return (TRUE);
+	while (liste->type == 2 && liste->type == 3 && liste->type == 4)
+	{
+		ft_putendl(liste->str);
+		ft_putnbr(liste->type);
+		if (liste->type == 5)
+			liste = liste->next;
+		str = liste->str;
+		tmp = liste->next;
+		while (ft_strcmp(str, tmp->str) == 0)
+			tmp = tmp->next;
+		liste = liste->next;
+	}
+	if (tmp->type == 6)
+		return (TRUE);
+	return (FALSE);
 }
 
 int		check_name_room(t_liste **liste)
